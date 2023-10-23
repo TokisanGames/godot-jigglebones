81dd4228ce50a409b179f4b36cd0134d554e4ea5
mikeysax <michaelmerrittfarrell@gmail.com>
2023-10-22 21:31:09 -0400

    Fixed parent node

--- addons/jigglebones/custom_node.gd
+++ addons/jigglebones/custom_node.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here
 	# Add the new type with a name, a parent type, a script and an icon
-	add_custom_type("Jigglebone", "Spatial", preload("jigglebone.gd"), preload("icon.svg"))
+	add_custom_type("Jigglebone", "Node3D", preload("jigglebone.gd"), preload("icon.svg"))
 
 func _exit_tree():
 	# Clean-up of the plugin goes here
